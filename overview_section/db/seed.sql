f61d0b77f9e39c3472507b1834299580b82875b2
greyes92 <gibranreyes92@gmail.com>
2022-06-28 02:20:04 -0500

    collapsible function added, css still needs some work

--- overview_section/db/seed.sql
+++ overview_section/db/seed.sql
@@ -9,14 +9,10 @@ VALUES (
 
 
 INSERT INTO product_features (features_body, sku) 
-VALUES ('Trigger nostalgic memories as you build this wonderfully detailed LEGO® brick Nintendo Entertainment System™ (71374) and interactive, 1980s-style television displaying the classic Super Mario Bros.™ game
-
-The TV has a handle-operated scrolling screen and if you scan the action brick by placing LEGO® Mario™ (figure not included) in the slot on top, he will react to the on-screen enemies, obstacles and power-ups
-
-Authentic details of the NES console are recreated in LEGO® style, including a controller and an opening slot for the buildable Game Pak with a realistic locking function to delight Super Mario Bros.™ fans
-
-Whether you were an NES gamer back in the day, a fan of retro stuff, or are just looking for an immersive, fun, creative DIY project to leave you feeling revitalized, this 2,646-piece model building kit is ideal for you
-
+VALUES (E'Trigger nostalgic memories as you build this wonderfully detailed LEGO® brick Nintendo Entertainment System™ (71374) and interactive, 1980s-style television displaying the classic Super Mario Bros.™ game\n
+The TV has a handle-operated scrolling screen and if you scan the action brick by placing LEGO® Mario™ (figure not included) in the slot on top, he will react to the on-screen enemies, obstacles and power-ups\n
+Authentic details of the NES console are recreated in LEGO® style, including a controller and an opening slot for the buildable Game Pak with a realistic locking function to delight Super Mario Bros.™ fans\n
+Whether you were an NES gamer back in the day, a fan of retro stuff, or are just looking for an immersive, fun, creative DIY project to leave you feeling revitalized, this 2,646-piece model building kit is ideal for you\n
 The buildable TV measures over 8” (22.5cm) high, 9” (23.5cm) wide and 6” (16cm) deep, and alongside the NES model makes a cool display item for your home or office. It also makes the best LEGO® gift for gamer friends',
 '6434148');
 
